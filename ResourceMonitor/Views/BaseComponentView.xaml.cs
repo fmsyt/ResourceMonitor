2ac3ee25a7744076ba8bf1187f36569679a1cdd4
fmsyt <fms20yt@gmail.com>
2021-12-30 23:16:08 +0900

    refactoring

--- ResourceMonitor/Views/BaseComponentView.xaml.cs
+++ ResourceMonitor/Views/BaseComponentView.xaml.cs
@@ -25,68 +25,38 @@ namespace ResourceMonitor.Views
     /// </summary>
     public partial class BaseComponentView : UserControl
     {
-        private DispatcherTimer? _timer = null;
-
-        private MemoryViewModel memory;
-
-        private Label label = new Label();
-        private Label current = new Label();
+        private readonly CpuViewModel cpu;
+        private readonly MemoryViewModel memory;
 
         public BaseComponentView()
         {
             InitializeComponent();
 
+            this.cpu = new CpuViewModel();
             this.memory = new MemoryViewModel();
 
             this.Loaded += new RoutedEventHandler(UserControl_Loaded);
 
-            label.Content = "CPU";
-            current.Content = Cpu.Instance.Current();
+            Grid.SetRow(cpu.Label, 0);
+            Grid.SetRow(cpu.Current, 0);
+            Grid.SetColumn(cpu.Label, 0);
+            Grid.SetColumn(cpu.Current, 1);
 
-            Grid.SetColumn(label, 0);
-            Grid.SetColumn(current, 1);
+            Grid.SetRow(memory.Label, 1);
+            Grid.SetRow(memory.Current, 1);
+            Grid.SetColumn(memory.Label, 0);
+            Grid.SetColumn(memory.Current, 1);
 
-            Grid.SetColumn(memory.Label, 2);
-            Grid.SetColumn(memory.Current, 3);
-
-            this.Panel.Children.Add(label);
-            this.Panel.Children.Add(current);
+            this.Panel.Children.Add(cpu.Label);
+            this.Panel.Children.Add(cpu.Current);
 
             this.Panel.Children.Add(memory.Label);
             this.Panel.Children.Add(memory.Current);
         }
 
-        private void BaseComponentView_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
-        {
-            StopTimer();
-        }
-
-        private void SetupTimer()
-        {
-            _timer = new DispatcherTimer();
-            _timer.Interval = new TimeSpan(0, 0, 1);
-            _timer.Tick += new EventHandler(UpdateCurrentContent);
-            _timer.Start();
-
-            var window = Window.GetWindow(this);
-            window.Closing += new CancelEventHandler(BaseComponentView_Closing);
-        }
-
-        
-
-        private void UpdateCurrentContent(object sender, EventArgs e)
-        {
-            current.Content = Cpu.Instance.Current();
-        }
-
-        private void StopTimer()
-        {
-            if (_timer != null) { _timer.Stop(); }
-        }
-
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            SetupTimer();
+            this.cpu.SetupTimer();
             this.memory.SetupTimer();
         }
     }
